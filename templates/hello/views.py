f5b870bb7827128dddf1ade257a1d2ce84a1640b
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-03-25 02:13:17 -0600

    added route profile
    Signed-off-by: Cesar Augusto Garcia Perez <cesarau04@gmail.com>

--- templates/hello/views.py
+++ templates/hello/views.py
@@ -1,8 +1,15 @@
 from flask import render_template, Blueprint
 
 hello_blueprint = Blueprint('hello', __name__)
+user_profile = Blueprint('user_profile', __name__)
+
+# Puede ser util en el futuro
+# https://stackoverflow.com/questions/30620276/flask-and-react-routing
 
 @hello_blueprint.route('/')
-# @hello_blueprint.route('/hello')
+def index():
+    return render_template('index.html')
+
+@user_profile.route('/profile')
 def index():
     return render_template('index.html')
