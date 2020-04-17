48a5bfa7060f597df8099d5e16f24b60c85d55f8
Fernando Partida <ferpart@hotmail.com>
2020-04-17 17:33:17 -0500

    index files created for ease of access

---
+++ templates/static/js/components/index.jsx
@@ -0,0 +1,7 @@
+import BookingBox from './BookingBox';
+import NavBar from './NavBar';
+
+export default {
+    BookingBox,
+    NavBar
+}
--- templates/static/js/routes.js
+++ templates/static/js/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { HashRouter, Route, hashHistory, BrowserRouter } from 'react-router-dom';
-import Home from './views/Home/Home';
-import Profile from './views/Profile/Profile'
+import { Route, hashHistory, BrowserRouter } from 'react-router-dom';
+import Home from 'views/Home';
+import Profile from 'views/Profile'
+import NavBar from 'components/NavBar'
 
 // import more components
 export default (
     <BrowserRouter history={hashHistory}>
      <div>
+      <NavBar/>
       <Route exact path='/' component={Home} />
       <Route path='/profile' component={Profile} />
      </div>
