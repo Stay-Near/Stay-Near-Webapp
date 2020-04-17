062248f7196f5e17895700a30b2e86579d9b1068
Fernando Partida <ferpart@hotmail.com>
2020-04-17 17:34:07 -0500

    remove Nav bar and add styles

--- templates/static/js/views/Home/Home.jsx
+++ templates/static/js/views/Home/Home.jsx
@@ -1,22 +1,22 @@
 // HomePage
 import React, { Component } from 'react';
 
-import styles from "./Home.css";
-import NavBar from '../../components/NavBar/NavBar'
-import BookingBox from '../../components/BookingBox/BookingBox'
+//import styles from "./Home.css";
+import BookingBox from 'components/BookingBox'
+import styles from './Home.css'
 
 export default class Home extends Component {
    render() {
       return (
+         <div>
             <div className={styles.bg}>
-               <NavBar/>
                <div className="row">
-                  <div className={styles.pad}>
+                  <div className={styles.bb}>
                      <BookingBox/>
                   </div>
                </div>
-
             </div>
+         </div>
        )
    }
 };
